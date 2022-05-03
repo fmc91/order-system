bbe41bb6dae183a2e9a8423540261089128af693
Fazal Cheema <fazal.cheema@live.co.uk>
2022-05-03 20:52:32 +0100

    Pull CRUD methods for Region up to interface

--- DomainLayer/Customer/ICustomerService.cs
+++ DomainLayer/Customer/ICustomerService.cs
@@ -5,10 +5,14 @@ namespace DomainLayer
     public interface ICustomerService
     {
         Task<Customer> CreateCustomerAsync(Customer customer);
+        Task<Region> CreateRegionAsync(Region region);
         Task<IList<Customer>> GetAllCustomersAsync(int page, int itemsPerPage);
+        Task<IList<Region>> GetAllRegionsAsync(int page, int itemsPerPage);
         Task<Customer> GetCustomerByIdAsync(int customerId);
         Task<IList<Customer>> GetCustomersByNameSearchAsync(string query, int page, int itemsPerPage);
         Task<IList<Customer>> GetCustomersByRegionAsync(int regionId, int page, int itemsPerPage);
+        Task<Region> GetRegionByIdAsync(int regionId);
         Task UpdateCustomerAsync(Customer customer);
+        Task UpdateRegionAsync(Region region);
     }
 }
