52c167c2d332f2a4b700a4439ccfd2442ec4a602
Fazal Cheema <fazal.cheema@live.co.uk>
2022-05-07 13:34:11 +0100

    Create RepositoryProvider class

---
+++ OrderSystem/RepositoryProvider.cs
@@ -0,0 +1,19 @@
+﻿using DataLayer;
+
+namespace OrderSystem
+{
+    public class RepositoryProvider
+    {
+        private readonly IServiceProvider _services;
+
+        public RepositoryProvider(IServiceProvider services)
+        {
+            _services = services;
+        }
+
+        public IRepository<TModel, TEntity> GetRepository<TModel, TEntity>() where TEntity : class
+        {
+            return _services.GetRequiredService<IRepository<TModel, TEntity>>();
+        }
+    }
+}
