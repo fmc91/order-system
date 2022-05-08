b269343c1d875d66c093fd19721494e2ad1db1b0
Fazal Cheema <fazal.cheema@live.co.uk>
2022-05-08 14:41:30 +0100

    Refactor CategoryController to use generic repository

--- OrderSystem/Controllers/CategoryController.cs
+++ OrderSystem/Controllers/CategoryController.cs
@@ -1,7 +1,8 @@
-﻿using DomainLayer;
-using DomainLayer.ProductModel;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using DataLayer;
+using DataLayer.Model;
+using OrderSystem.Model;
 
 namespace OrderSystem.Controllers
 {
@@ -9,92 +10,92 @@ namespace OrderSystem.Controllers
     [ApiController]
     public class CategoryController : ControllerBase
     {
-        private readonly IProductService _productService;
+        private readonly IRepository<Category> _categoryRepository;
 
-        public CategoryController(IProductService productService)
+        private readonly IRepository<Product> _productRepository;
+
+        public CategoryController(RepositoryProvider repositoryProvider)
         {
-            _productService = productService;
+            _categoryRepository = repositoryProvider.GetRepository<Category>();
+            _productRepository = repositoryProvider.GetRepository<Product>();
         }
 
         [HttpGet]
         public async Task<IActionResult> GetAllCategoriesAsync(int page = 0, int itemsPerPage = 20)
         {
-            var result = await _productService.GetAllCategoriesAsync(page, itemsPerPage);
+            var result = await _categoryRepository.QueryAsync<CategoryModel>(x => x
+                .OrderBy(c => c.Name)
+                .Skip(page * itemsPerPage)
+                .Take(itemsPerPage));
+
             return Ok(result);
         }
 
         [HttpGet("search")]
         public async Task<IActionResult> GetCategoriesByNameSearchAsync(string query, int page = 0, int itemsPerPage = 20)
         {
-            var result = await _productService.GetCategoriesByNameSearchAsync(query, page, itemsPerPage);
+            var result = await _categoryRepository.QueryAsync<CategoryModel>(x => x
+                .Where(c => c.Name.Contains(query))
+                .OrderBy(x => x.Name)
+                .Skip(page * itemsPerPage)
+                .Take(itemsPerPage));
+
             return Ok(result);
         }
 
         [HttpGet("{id:int}")]
         public async Task<IActionResult> GetCategoryAsync(int id)
         {
-            try
-            {
-                var result = await _productService.GetCategoryByIdAsync(id);
-                return Ok(result);
-            }
-            catch (EntityNotFoundException ex)
-            {
-                return NotFound(new { errorMessage = ex.Message });
-            }
+            if (!await _categoryRepository.ExistsAsync(id))
+                return NotFound();
+
+            var result = await _categoryRepository.GetByIdAsync<CategoryModel>(id);
+            return Ok(result);
         }
 
         [HttpGet("{id:int}/product")]
         public async Task<IActionResult> GetProductsByCategoryAsync(int id, int page = 0, int itemsPerPage = 20)
         {
-            var result = await _productService.GetProductsByCategoryAsync(id, page, itemsPerPage);
+            var result = await _productRepository.QueryAsync<ProductModel>(x => x
+                .Where(p => p.CategoryId == id)
+                .OrderBy(p => p.Name)
+                .Skip(page * itemsPerPage)
+                .Take(itemsPerPage));
+
             return Ok(result);
         }
 
         [HttpPost]
-        public async Task<IActionResult> CreateCategoryAsync([FromBody] Category category)
+        public async Task<IActionResult> CreateCategoryAsync([FromBody] CategoryModel category)
         {
-            try
-            {
-                var result = await _productService.CreateCategoryAysnc(category);
-                return Ok(result);
-            }
-            catch (InvalidOperationException ex)
-            {
-                return BadRequest(new { errorMessage = ex.Message });
-            }
+            if (category.CategoryId != 0)
+                return BadRequest();
+
+            var result = await _productRepository.AddAsync(category);
+            return CreatedAtAction("GetCategory", new { id = result.CategoryId }, category);
         }
 
         [HttpPut]
-        public async Task<IActionResult> UpdateCategoryAsync([FromBody] Category category)
+        public async Task<IActionResult> UpdateCategoryAsync([FromBody] CategoryModel category)
         {
-            try
-            {
-                await _productService.UpdateCategoryAsync(category);
-                return NoContent();
-            }
-            catch (EntityNotFoundException ex)
-            {
-                return NotFound(new { errorMessage = ex.Message });
-            }
-            catch (InvalidOperationException ex)
-            {
-                return BadRequest(new { errorMessage = ex.Message });
-            }
+            if (category.CategoryId == 0)
+                return BadRequest();
+
+            if (!await _categoryRepository.ExistsAsync(category.CategoryId))
+                return NotFound();
+
+            await _categoryRepository.UpdateAsync(category.CategoryId, category);
+            return NoContent();
         }
 
         [HttpDelete("{id:int}")]
         public async Task<IActionResult> RemoveCategoryAsync(int id)
         {
-            try
-            {
-                await _productService.RemoveCategoryAsync(id);
-                return NoContent();
-            }
-            catch (EntityNotFoundException ex)
-            {
-                return NotFound(new { errorMessage = ex.Message });
-            }
+            if (!await _categoryRepository.ExistsAsync(id))
+                return NotFound();
+
+            await _categoryRepository.RemoveAsync(id);
+            return NoContent();
         }
     }
 }
