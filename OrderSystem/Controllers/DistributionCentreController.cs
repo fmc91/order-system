d580954d18bdec4584f2e669c131c485b8d137b8
Fazal Cheema <fazal.cheema@live.co.uk>
2022-05-08 16:36:00 +0100

    Refactor DistributionCentreController to use generic repository

--- OrderSystem/Controllers/DistributionCentreController.cs
+++ OrderSystem/Controllers/DistributionCentreController.cs
@@ -1,7 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using DomainLayer;
-using DomainLayer.DistributionCentreModel;
+using DataLayer;
+using DataLayer.Model;
+using OrderSystem.Model;
 
 namespace OrderSystem.Controllers
 {
@@ -9,19 +10,29 @@ namespace OrderSystem.Controllers
     [ApiController]
     public class DistributionCentreController : ControllerBase
     {
-        private readonly IDistributionCentreService _distributionCentreService;
+        private readonly IRepository<DistributionCentre> _distributionCentreRepository;
 
-        public DistributionCentreController(IDistributionCentreService distributionCentreService)
+        private readonly IRepository<StockItem> _stockItemRepository;
+
+        public DistributionCentreController(RepositoryProvider repositoryProvider)
         {
-            _distributionCentreService = distributionCentreService;
+            _distributionCentreRepository = repositoryProvider.GetRepository<DistributionCentre>();
+            _stockItemRepository = repositoryProvider.GetRepository<StockItem>();
         }
 
         [HttpGet]
         public async Task<IActionResult> GetAllDistributionCentresAsync(string? country = null, int page = 0, int itemsPerPage = 20)
         {
             var result = country == null ?
-                await _distributionCentreService.GetAllDistributionCentresAsync(page, itemsPerPage) :
-                await _distributionCentreService.GetDistributionCentresByCountryCodeAsync(country, page, itemsPerPage);
+                await _distributionCentreRepository.QueryAsync<DistributionCentreModel>(x => x
+                    .OrderBy(c => c.Name)
+                    .Skip(page * itemsPerPage)
+                    .Take(itemsPerPage)) :
+                await _distributionCentreRepository.QueryAsync<DistributionCentreModel>(x => x
+                    .Where(c => c.DistributionCentreAddress.Address.Country.CountryCode == country)
+                    .OrderBy(c => c.Name)
+                    .Skip(page * itemsPerPage)
+                    .Take(itemsPerPage));
 
             return Ok(result);
         }
@@ -29,61 +40,61 @@ namespace OrderSystem.Controllers
         [HttpGet("search")]
         public async Task<IActionResult> GetDistributionCentresByNameSearchAsync(string query, int page = 0, int itemsPerPage = 20)
         {
-            var result = await _distributionCentreService.GetDistributionCentresByNameSearchAsync(query, page, itemsPerPage);
+            var result = await _distributionCentreRepository.QueryAsync<DistributionCentreModel>(x => x
+                .Where(c => c.Name.Contains(query))
+                .OrderBy(c => c.Name)
+                .Skip(page * itemsPerPage)
+                .Take(itemsPerPage));
+
             return Ok(result);
         }
 
         [HttpGet("{id:int}")]
         public async Task<IActionResult> GetDistributionCentreAsync(int id)
         {
-            try
-            {
-                var result = await _distributionCentreService.GetDistributionCentreByIdAsync(id);
-                return Ok(result);
-            }
-            catch (EntityNotFoundException ex)
-            {
-                return NotFound(new { errorMessage = ex.Message });
-            }
+            if (!await _distributionCentreRepository.ExistsAsync(id))
+                return NotFound();
+
+            var result = _distributionCentreRepository.GetByIdAsync<DistributionCentreModel>(id);
+            return Ok(result);
         }
 
         [HttpGet("{id:int}/stock-item")]
         public async Task<IActionResult> GetStockItemsByDistributionCentreAsync(int id, int page = 0, int itemsPerPage = 20)
         {
-            var result = await _distributionCentreService.GetStockItemsByDistributionCentreAsync(id, page, itemsPerPage);
+            if (!await _distributionCentreRepository.ExistsAsync(id))
+                return NotFound();
+
+            var result = await _stockItemRepository.QueryAsync<StockItemModel>(x => x
+                .Where(i => i.DistributionCentreId == id)
+                .OrderBy(i => i.Product.Name)
+                .Skip(page * itemsPerPage)
+                .Take(itemsPerPage));
+
             return Ok(result);
         }
 
         [HttpPost]
-        public async Task<IActionResult> CreateDistributionCentreAsync([FromBody] DistributionCentre distributionCentre)
+        public async Task<IActionResult> CreateDistributionCentreAsync([FromBody] DistributionCentreModel distributionCentre)
         {
-            try
-            {
-                var result = await _distributionCentreService.CreateDistributionCentreAsync(distributionCentre);
-                return CreatedAtAction("GetDistributionCentre", new { id = result.DistributionCentreId }, result);
-            }
-            catch (InvalidOperationException ex)
-            {
-                return BadRequest(new { errorMessage = ex.Message });
-            }
+            if (distributionCentre.DistributionCentreId != 0)
+                return BadRequest();
+
+            var result = await _distributionCentreRepository.AddAsync(distributionCentre);
+            return CreatedAtAction("GetDistributionCentre", new { id = result.DistributionCentreId }, result);
         }
 
         [HttpPut]
-        public async Task<IActionResult> UpdateDistributionCentreAsync([FromBody] DistributionCentre distributionCentre)
+        public async Task<IActionResult> UpdateDistributionCentreAsync([FromBody] DistributionCentreModel distributionCentre)
         {
-            try
-            {
-                await _distributionCentreService.UpdateDistributionCentreAsync(distributionCentre);
-                return NoContent();
-            }
-            catch (EntityNotFoundException ex)
-            {
-                return NotFound(new { errorMessage = ex.Message });
-            }
-            catch (InvalidOperationException ex)
-            {
-                return BadRequest(new { errorMessage = ex.Message });
-            }
+            if (distributionCentre.DistributionCentreId == 0)
+                return BadRequest();
+
+            if (!await _distributionCentreRepository.ExistsAsync(distributionCentre.DistributionCentreId))
+                return NotFound();
+
+            await _distributionCentreRepository.UpdateAsync(distributionCentre.DistributionCentreId, distributionCentre);
+            return NoContent();
         }
     }
 }
